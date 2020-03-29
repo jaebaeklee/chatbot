3d7b4244c06194922aa8f0e1710b25571f1ef0b1
jaebaeklee <45986313+jaebaeklee@users.noreply.github.com>
2020-03-28 21:32:02 -0500

    Update ChatBot.java

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,4 +1,3 @@
-//AV KEY = A406SN7EO3N2Y9FJ
 public class ChatBot
 {
     public static void main(String[] args) throws Exception
@@ -6,7 +5,6 @@ public class ChatBot
         bot ChatBot = new bot();
         ChatBot.setVerbose(true);
         ChatBot.connect("irc.freenode.net");
-        // Name of channel is testChannel
         ChatBot.joinChannel("#chatBotTest");
         ChatBot.sendMessage("#chatBotTest", "Enter 'weather' followed by zipcode, city or 'symbol' followed by a stock symbol to return the temperature or finances.");
     }
